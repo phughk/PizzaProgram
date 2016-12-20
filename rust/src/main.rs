93646a02523ce9febc9b0a1e35bb2f02e3a9caeb
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2016-12-20 23:35:53 +0000

    Working hello name in rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use std::string::String;
+use std::io::Write; // Used for flush implicitly?
+
 fn main() {
-    println!("Hello");
+    let name  = get_name();
+    println!("Hello {}", name);
+}
+
+fn get_name() -> String {
+    print!("Enter name: ");
+    io::stdout().flush();
+    let stdin = io::stdin();
+    let mut name = String::new();
+    stdin.read_line(&mut name);
+    return name;
 }
