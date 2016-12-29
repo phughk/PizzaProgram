8ee588152f26a78bddf4c07dc77f7282bff2d5f7
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2016-12-29 21:54:00 +0000

    Builds, but boost not working

---
+++ cmake/full_clean.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+make clean
+rm Makefile, cmake_instal.cmake, CMakeCache.txt
+rm -Rf MakeFiles
+rm conanbuildinfo.cmake, conaninfo.txt
--- cmake/pizza.cpp
+++ cmake/pizza.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <boost/asio.hpp>
+//#include <boost/asio.hpp>
 
 using namespace std;
 
 int main() {
 	cout << "Works" << endl;
+	return 0;
 }
