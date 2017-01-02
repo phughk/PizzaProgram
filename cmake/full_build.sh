4d2cf86c884634272f617cafa42f241735a11153
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2017-01-02 21:10:34 +0000

    Fixup for clean and build scripts in cmake

---
+++ cmake/full_build.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+conan install
+cmake .
+make all
--- cmake/full_clean.sh
+++ cmake/full_clean.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 make clean
-rm Makefile, cmake_instal.cmake, CMakeCache.txt
-rm -Rf MakeFiles
-rm conanbuildinfo.cmake, conaninfo.txt
+rm Makefile cmake_install.cmake CMakeCache.txt
+rm -Rf CMakeFiles bin
+rm conanbuildinfo.cmake conaninfo.txt
