cf9a6a3e4bb691718c852b98d117aae169fde9cb
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2017-01-05 05:48:56 +0000

    Iterated values passed as input

--- ng2/src/app/pizza-topping/pizza-topping.component.ts
+++ ng2/src/app/pizza-topping/pizza-topping.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-pizza-topping',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pizza-topping.component.css']
 })
 export class PizzaToppingComponent implements OnInit {
+
+	@Input() name: string;
 
   constructor() { }
 
