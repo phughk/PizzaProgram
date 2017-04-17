3de121e941c2c87b201a1074fcec0ccacc6cd2a8
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2017-04-18 00:37:58 +0100

    Haskell - Figure out types

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,3 +1,9 @@
+data PizzaBase = Thin | Thick
+data Topping = Tomatoe | Sweetcorn | Peppers | Olives
+type Pizza = (PizzaBase, [Topping])
+
+pizzaA :: Pizza
+pizzaA = (Thin, [Tomatoe, Sweetcorn])
 
 main = do
 	inputdata <- getContents
