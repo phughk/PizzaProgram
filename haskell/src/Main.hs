bb2ab2b7a1556e59effeab40b191424f5be020a5
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2017-04-18 01:32:00 +0100

    Haskell - string formttting for toppings

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,10 +1,22 @@
-data PizzaBase = Thin | Thick
-data Topping = Tomatoe | Sweetcorn | Peppers | Olives
+import Data.List
+data PizzaBase = Thin | Thick deriving Show
+data Topping = Tomatoe | Sweetcorn | Peppers | Olives deriving Show
 type Pizza = (PizzaBase, [Topping])
 
 pizzaA :: Pizza
 pizzaA = (Thin, [Tomatoe, Sweetcorn])
 
+addTopping :: Pizza -> Topping -> Pizza
+addTopping (base, toppings) topping = (base, toppings ++ [topping])
+
+pizzaB = addTopping pizzaA Peppers
+
+strToppings :: [Topping] -> String
+strToppings toppings = concat (intersperse ", " (map show toppings))
+strPizza :: Pizza -> String
+strPizza (base, toppings) = (show base) ++ strToppings toppings
+
 main = do
 	inputdata <- getContents
+	putStrLn $ strPizza pizzaB
 	putStrLn $ inputdata
