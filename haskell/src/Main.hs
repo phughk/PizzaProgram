3a6f5ce4caf6c185e34e8356f9a7b0ba3113a14a
Przemyslaw Hugh Kaznowski <hugh@kaznowski.com>
2017-05-07 01:16:59 +0100

    Some sort of haskell progress, no idea

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,20 +1,25 @@
 import Data.List
-data PizzaBase = Thin | Thick deriving Show
-data Topping = Tomatoe | Sweetcorn | Peppers | Olives deriving Show
+data PizzaBase = Thin | Thick deriving (Enum, Show)
+data Topping = Tomatoe | Sweetcorn | Peppers | Olives deriving (Enum, Show)
 type Pizza = (PizzaBase, [Topping])
 
 pizzaA :: Pizza
 pizzaA = (Thin, [Tomatoe, Sweetcorn])
 
-addTopping :: Pizza -> Topping -> Pizza
-addTopping (base, toppings) topping = (base, toppings ++ [topping])
+addTopping :: Pizza -> [Topping] -> Pizza
+addTopping (base, toppings) toppingList = (base, toppings ++ toppingList)
 
-pizzaB = addTopping pizzaA Peppers
+pizzaB = addTopping pizzaA [Peppers, Olives]
 
 strToppings :: [Topping] -> String
 strToppings toppings = concat (intersperse ", " (map show toppings))
 strPizza :: Pizza -> String
 strPizza (base, toppings) = (show base) ++ strToppings toppings
+
+pickABaseFromList :: Int -> [PizzaBase] -> PizzaBase
+pickABaseFromList index bases = bases !! index
+
+pickABase = (\index -> pickABaseFromList index [Thin ..])
 
 main = do
 	inputdata <- getContents
